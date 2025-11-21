b09a4d116894181755d25f0613c29f7d23a4bc37
Fernanda Marques <129966542+FernandaMarques07@users.noreply.github.com>
2025-11-21 11:59:36 -0300

    ajustes js - funcionalidades

--- js/main.js
+++ js/main.js
@@ -286,6 +286,28 @@
                 e.stopPropagation(); // Impede que o clique no botão active o box
             });
         });
+// funcionalidades 
+
+  /* AjaxChimp
+	* ------------------------------------------------------ */
+	var ssAjaxChimp = function() {
+
+		$('#mc-form').ajaxChimp({
+			language: 'es',
+		   url: cfg.mailChimpURL
+		});
+
+		$.ajaxChimp.translations.es = {
+		  'submit': 'Submitting...',
+		  0: '<i class="fa fa-check"></i> We have sent you a confirmation email',
+		  1: '<i class="fa fa-warning"></i> You must enter a valid e-mail address.',
+		  2: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+		  3: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+		  4: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+		  5: '<i class="fa fa-warning"></i> E-mail address is not valid.'
+		} 
+
+	};
 
 // Sistema de tabs
 document.querySelectorAll('.feature-box').forEach(button => {
@@ -351,3 +373,4 @@ form.addEventListener('submit', async (e) => {
         msg.innerHTML = "Erro ao enviar. Tente novamente.";
     }
 });
+

63e8e60cd02f764a6958bfc5b704a0b679b8cb9c
Fernanda Marques <marxsnanda07@gmail.com>
2025-11-21 12:37:22 -0300

    funcionalidades ok

--- js/main.js
+++ js/main.js
@@ -178,6 +178,30 @@
 
 	};
 
+
+  /* AjaxChimp
+	* ------------------------------------------------------ */
+	var ssAjaxChimp = function() {
+
+		$('#mc-form').ajaxChimp({
+			language: 'es',
+		   url: cfg.mailChimpURL
+		});
+
+		$.ajaxChimp.translations.es = {
+		  'submit': 'Submitting...',
+		  0: '<i class="fa fa-check"></i> We have sent you a confirmation email',
+		  1: '<i class="fa fa-warning"></i> You must enter a valid e-mail address.',
+		  2: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+		  3: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+		  4: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+		  5: '<i class="fa fa-warning"></i> E-mail address is not valid.'
+		} 
+
+	};
+
+
+ 
   /* Back to Top
 	* ------------------------------------------------------ */
 	var ssBackToTop = function() {
@@ -286,28 +310,6 @@
                 e.stopPropagation(); // Impede que o clique no botão active o box
             });
         });
-// funcionalidades 
-
-  /* AjaxChimp
-	* ------------------------------------------------------ */
-	var ssAjaxChimp = function() {
-
-		$('#mc-form').ajaxChimp({
-			language: 'es',
-		   url: cfg.mailChimpURL
-		});
-
-		$.ajaxChimp.translations.es = {
-		  'submit': 'Submitting...',
-		  0: '<i class="fa fa-check"></i> We have sent you a confirmation email',
-		  1: '<i class="fa fa-warning"></i> You must enter a valid e-mail address.',
-		  2: '<i class="fa fa-warning"></i> E-mail address is not valid.',
-		  3: '<i class="fa fa-warning"></i> E-mail address is not valid.',
-		  4: '<i class="fa fa-warning"></i> E-mail address is not valid.',
-		  5: '<i class="fa fa-warning"></i> E-mail address is not valid.'
-		} 
-
-	};
 
 // Sistema de tabs
 document.querySelectorAll('.feature-box').forEach(button => {
@@ -349,50 +351,3 @@ document.querySelectorAll('.play-btn').forEach(button => {
     console.log('Reproduzindo áudio:', audioType);
   });
 });
-
-/* EMAIL JS */
-const form = document.getElementById('contact-form');
-const msg = document.querySelector('.form-message');
-
-form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-
-    const formData = new FormData(form);
-
-    msg.innerHTML = "Enviando...";
-
-    const response = await fetch(form.action, {
-        method: 'POST',
-        body: formData
-    });
-
-    if (response.ok) {
-        msg.innerHTML = "Mensagem enviada!";
-        form.reset();
-    } else {
-        msg.innerHTML = "Erro ao enviar. Tente novamente.";
-    }
-});
-
-// seleção funcionalidades
-
-  /* AjaxChimp
-	* ------------------------------------------------------ */
-	var ssAjaxChimp = function() {
-
-		$('#mc-form').ajaxChimp({
-			language: 'es',
-		   url: cfg.mailChimpURL
-		});
-
-		$.ajaxChimp.translations.es = {
-		  'submit': 'Submitting...',
-		  0: '<i class="fa fa-check"></i> We have sent you a confirmation email',
-		  1: '<i class="fa fa-warning"></i> You must enter a valid e-mail address.',
-		  2: '<i class="fa fa-warning"></i> E-mail address is not valid.',
-		  3: '<i class="fa fa-warning"></i> E-mail address is not valid.',
-		  4: '<i class="fa fa-warning"></i> E-mail address is not valid.',
-		  5: '<i class="fa fa-warning"></i> E-mail address is not valid.'
-		} 
-
-	};
